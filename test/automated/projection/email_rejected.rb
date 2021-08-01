256b5d9386accf7b5148de7513b89790bc9db329
Joseph Choe <joseph@josephchoe.com>
2021-08-01 06:56:39 -0600

    Projection tests fixed

--- test/automated/projection/email_rejected.rb
+++ test/automated/projection/email_rejected.rb
@@ -9,15 +9,16 @@ context "Projection" do
     email_rejected = Controls::Events::EmailRejected.example
 
     registration_id = email_rejected.registration_id or fail
+    email_rejected_time_iso8601 = email_rejected.time or fail
 
     Projection.(registration, email_rejected)
 
     test "ID is set" do
-      assert(registration.id == email_rejected.registration_id)
+      assert(registration.id == registration_id)
     end
 
     test "Email Rejected time is converted and copied" do
-      email_rejected_time = Time.parse(email_rejected.time)
+      email_rejected_time = Time.parse(email_rejected_time_iso8601)
 
       assert(registration.email_rejected_time == email_rejected_time)
     end
--- test/automated/projection/registered.rb
+++ test/automated/projection/registered.rb
@@ -9,15 +9,16 @@ context "Projection" do
     registered = Controls::Events::Registered.example
 
     registration_id = registered.registration_id or fail
+    registered_time_iso8601 = registered.time or fail
 
     Projection.(registration, registered)
 
     test "ID is set" do
-      assert(registration.id == registered.registration_id)
+      assert(registration.id == registration_id)
     end
 
     test "Registered time is converted and copied" do
-      registered_time = Time.parse(registered.time)
+      registered_time = Time.parse(registered_time_iso8601)
 
       assert(registration.registered_time == registered_time)
     end
